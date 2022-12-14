a94d641ab4cc94773db674004e8f9ec149ae9f58
Vinay Singh <vinayksingh2@gmail.com>
2022-12-14 11:44:11 +0530

    user crud api

--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"gin-mongo-boilerplate/configs"
+
+	"gin-mongo-boilerplate/routes"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"data": "Hello from Gin-gonic & mongoDB",
-		})
-	})
+	configs.ConnectDB()
+
+	routes.UserRoute(router)
 
 	router.Run("localhost:6000")
 }
